8698aaacecaa4cde0dc445bd39e4cbbdec06bf1f
up649230 <up649230@myport.ac.uk>
2016-08-27 15:43:33 +0100

    Help page added, styling changes

--- appengine/static/blocks/pystart.js
+++ appengine/static/blocks/pystart.js
@@ -1,27 +1,27 @@
 /**
- * @license
- * Visual Blocks Editor
- *
- * Copyright 2012 Google Inc.
- * https://developers.google.com/blockly/
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* @license
+* Visual Blocks Editor
+*
+* Copyright 2012 Google Inc.
+* https://developers.google.com/blockly/
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 /**
- * @fileoverview Start block for Blockly.
- * @author fraser@google.com (Neil Fraser)
- */
+* @fileoverview Start block for Blockly.
+* @author fraser@google.com (Neil Fraser)
+*/
 'use strict';
 
 goog.provide('Blockly.Blocks.pystart');
@@ -33,6 +33,7 @@ Blockly.Blocks['python_start'] = {
 
   init: function() {
     Blockly.BlockSvg.START_HAT = true;
+    // .appendField(new Blockly.FieldImage('../../media/play_icon.png', 32, 32, '*'));
     this.setNextStatement(true);
     this.setTooltip('The start block is not a true Python block and is only\
     there to indicate which blocks will be executed when "Run full" is\
--- appengine/static/python_compressed.js
+++ appengine/static/python_compressed.js
@@ -75,11 +75,11 @@ Blockly.Python.math_random_int=function(a){Blockly.Python.definitions_.import_ra
 
 Blockly.Python.COLOUR = {};
 Blockly.Python.COLOUR['notype'] = '#8B7D6B';
-Blockly.Python.COLOUR['int'] = '#dfdf20';   //yellow
 Blockly.Python.COLOUR['float'] = '#FF1919'; // red
-Blockly.Python.COLOUR['str'] = '#00CC33';  // green
+Blockly.Python.COLOUR['int'] = '#dfdf20';   //yellow
+Blockly.Python.COLOUR['str'] = '#00CC33' ; // blue
+Blockly.Python.COLOUR['range'] = '#0080FF';  // green
 Blockly.Python.COLOUR['bool'] = '#FF29FF'; // magenta
-Blockly.Python.COLOUR['range'] = '#0080FF' ; // blue
 Blockly.Python.RAINBOW = ['str', 'int', 'float', 'bool', 'range', 'str'];
 
 Blockly.Python.NEW_VARS = [
